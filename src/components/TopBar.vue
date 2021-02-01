615221b74f567a8ff665cfe6efae696b62c8d4b1
Henri Tukiainen <henri@momocode.com>
2021-02-01 04:45:39 +0200

    Add top bar

---
+++ src/components/TopBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="$style.component">
+    <img :src="$options.logoUrl" :class="$style.logo" />
+    <div :class="$style.title">
+      Welcome guide
+    </div>
+  </div>
+</template>
+
+<script>
+import logoUrl from '@/assets/images/helsinki-fi.svg'
+
+// TODO: use inline svg for logo and remove filter
+
+export default {
+  logoUrl
+}
+</script>
+
+<style module lang="scss">
+@use "~hds-design-tokens/lib/all" as hds;
+
+.component {
+  display: flex;
+  align-items: center;
+  padding: hds.$spacing-s;
+  background-color: hds.$color-copper-dark;
+
+  .logo {
+    display: inline-block;
+    margin-right: hds.$spacing-m;
+    height: 40px;
+    filter: invert(1) brightness(100);
+  }
+
+  .title {
+    display: inline-block;
+    line-height: 40px;
+    font-size: hds.$fontsize-heading-m;
+    font-weight: bold;
+    color: white;
+  }
+}
+</style>
--- src/demo/App.vue
+++ src/demo/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <TopBar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import TopBar from '@/components/TopBar'
+
+export default {
+  components: {
+    TopBar
+  }
+}
+</script>
