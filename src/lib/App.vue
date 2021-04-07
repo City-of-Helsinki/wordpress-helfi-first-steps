cdddd89c5692ea19ae6092c6cbfa512ad2331ec7
Henri Tukiainen <henri@momocode.com>
2021-04-07 17:19:11 +0300

    Update library app and library demo html

--- src/lib/App.vue
+++ src/lib/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app" :class="$style.app">
+    <TopBar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import TopBar from '@/components/TopBar'
+
+export default {
+  components: {
+    TopBar
+  }
+}
+</script>
 
 <style module lang="scss">
 .app {
--- src/lib/main.js
+++ src/lib/main.js
@@ -1,9 +1,12 @@
 /* global Vue */
 
+import VueCompositionApi from '@vue/composition-api'
 import App from './App'
 import router from '../router'
 
 import '../register-global-components'
+
+Vue.use(VueCompositionApi)
 
 export {App as component}
 export function mount (opts) {
